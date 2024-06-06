1424ddb82f192b1480867a6a7795e25655728a3d
Vishal Poudel <53789228+vishalpoudel@users.noreply.github.com>
2024-06-06 11:23:25 +0530

    Update test2.groovy

--- src/com/custom/test2.groovy
+++ src/com/custom/test2.groovy
@@ -1,12 +1,21 @@
 package com.custom;
 
 class test2 {
-
   int a
   String s
+  def script
+
+
+  
+test2(script)
+  {
+this.script=script
+    
+  }
+
 
  void func1(){
-echo "abc"
+     echo "abc"
     
     
   }
